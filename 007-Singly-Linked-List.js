8fa25ce23a5df861e4685492ac98302cb5432010
maxts0gt <bazarsadtsogtjargal@gmail.com>
2021-10-24 01:32:11 +0900

    add "remove duplicates feature"

--- 007-Singly-Linked-List.js
+++ 007-Singly-Linked-List.js
@@ -125,6 +125,19 @@ class SinglyLinkedList {
 		}
 		console.log(arr);
 	}
+	removeDuplicates(head) {
+		let current = head;
+
+		while (current !== null && current.next !== null) {
+			if (current.val === current.next.val) {
+				current.next = current.next.next;
+			} else {
+				current = current.next;
+			}
+		}
+
+		return head;
+	}
 }
 
 var list = new SinglyLinkedList();
